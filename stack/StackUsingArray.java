6adb7aa208df776c561c40f91714cf6088d563cb
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-31 23:19:34 +0530

    stack using array program aded

---
+++ stack/StackUsingArray.java
@@ -0,0 +1,57 @@
+package stack;
+
+public class StackUsingArray {
+	private int[] data;
+	private int top;
+ 
+	
+	private static final int DEFAULT_CAPACITY = 10;
+	public StackUsingArray() {
+		this(DEFAULT_CAPACITY);
+	}
+	
+	public StackUsingArray(int capacity) {
+		data = new int[capacity];
+		this.top = -1;
+	}
+	
+	public int size() {
+		return this.top + 1;
+	}
+	public Boolean isEmpty() {
+		return(this.size() == 0);
+	}
+	public void display() {
+		for(int i =top; i>=0; i--) {
+			System.out.print(data[i]+" ");
+		}
+		System.out.println("END");
+	}
+	
+	public void push(int value) throws Exception{
+		if(this.size() > data.length) {
+			throw new Exception("stack is full");
+		}
+		this.top++;
+		this.data[this.top] = value;
+		
+	}
+	
+	public void pop() throws Exception {
+		if(this.size() == 0) {
+			throw new Exception("stack is empty");
+		}
+		
+		this.data[this.top] = 0;
+		this.top--;
+	}
+	
+	public int topVal() throws Exception {
+		if(this.size() == 0) {
+			throw new Exception("stack is empty");
+		}
+		int rval = this.data[this.top];
+		return rval;
+		
+	}
+}
---
+++ stack/StackUsingArrayClient.java
@@ -0,0 +1,17 @@
+package stack;
+
+public class StackUsingArrayClient {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		StackUsingArray st = new StackUsingArray(5);
+		//st.push(20);
+		st.display();
+		for(int i= 1; i<=5;i++) {
+			st.push(i*10);
+		}
+		st.display();
+		System.out.println(st.topVal());
+	}
+
+}
