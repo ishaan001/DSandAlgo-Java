cf5f0e5ce4e7d97034759846c1981cac5a4baa2d
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-05 03:24:32 +0530

    A game of Numbers HackerEarth

--- stack/Bracket_is_Balanced.java
+++ stack/Bracket_is_Balanced.java
@@ -1,35 +1,36 @@
 package stack;
 
+import java.util.Scanner;
 import java.util.Stack;
 /*return true is paranthesis
  *  are balanced and false if they aren't
 */
 public class Bracket_is_Balanced {
 	
-	public Boolean isBalanced(String str) {
+	public static String isBalanced(String str) {
 		Stack<Character> stack = new Stack<>();
 		for(int i=0; i<str.length(); i++) {
 			if(str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '(' ) {
 				stack.push(str.charAt(i));
 			}else if( str.charAt(i) == '}' || str.charAt(i) == ']' || str.charAt(i) == ')' ) {
 				if(stack.isEmpty()) {
-					return false;
+					return "NO";
 				}
 				if( str.charAt(i) == '}') {
 					if(stack.peek() == '{') {
 						stack.pop();
 					}else
-						return false;
+						return "NO";
 				} else if( str.charAt(i) == ']') {
 					if(stack.peek() == '[') {
 						stack.pop();
 					}else
-						return false;
+						return "NO";
 				} else if( str.charAt(i) == ')') {
 					if(stack.peek() == '(') {
 						stack.pop();
 					}else
-						return false;
+						return "NO";
 				}
 			}else {
 				// Do nothing
@@ -37,17 +38,21 @@ public class Bracket_is_Balanced {
 			
 		}
 		if(stack.isEmpty()) {
-			return true;
+			return "YES";
 		}
 		else {
-			return false;
+			return "NO";
 		}
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "{[({a+B})]}";
-		Bracket_is_Balanced bb = new Bracket_is_Balanced();
-		System.out.println(bb.isBalanced(str));
+		  Scanner s = new Scanner(System.in);
+	        int test = s.nextInt();           
+	        // Write your code here
+	        for(int i=0;i<test;i++){
+	            String str = s.next();
+	            System.out.println(isBalanced(str));
+	            
+	        }
 
 	}
 	
