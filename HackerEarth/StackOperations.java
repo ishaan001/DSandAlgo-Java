a07fe8bbdcc2c90ff024a32c81dc539ba7af324c
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-06 06:49:27 +0530

    modification done according to stack

--- HackerEarth/StackOperations.java
+++ HackerEarth/StackOperations.java
@@ -20,16 +20,28 @@ public class StackOperations {
 		for(int i=arr.length-1; i>=0;i--) {
 			stack.push(arr[i]);
 		}
-		
 		int max =-1 ;
+		/*
 		for(int i=0; i<n&& i<k+1;i++)
-			if(i != k-1) max = Math.max(max,arr[i]);
-		 
+			if(i != k-1) max = Math.max(max,arr[i]);*/
+		 while(!stack.isEmpty() && count != k-1) {
+			 int r = stack.peek();
+			 stack.pop();
+			 count++;
+			 max = Math.max(max, r);
+		 }
 		
-		if(n==1 && k==1) {
+		if(n==1 && k==1){
+			System.out.println("-1");
+		}
+		else if(n==1 & max!=-1){
+			System.out.println(max);
+		}
+		else if(stack.isEmpty() && max == -1) {
 			System.out.println("-1");
 		} 
 		else {
+			 
 			System.out.println(max);
 		}
 
