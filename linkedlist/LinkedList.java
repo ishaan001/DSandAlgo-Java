833d5d7503bf4eedb9511fcf581c6b04f3d3d2bb
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-07 01:28:43 +0530

    addLast and addFirst method added

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -16,10 +16,52 @@ public class LinkedList {
 	
 	public void display() {
 		Node temp = head;
-		
+		System.out.println("-------------------------------");
 		while(temp != null) {
-			System.out.println(temp.data+" ");
+			System.out.print(temp.data+"-->");
 			temp = temp.next;
+		}
+		System.out.print("END.");
+		System.out.println("\n-------------------------------");
+	}
+	
+	public void addLast(int item) {
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		
+		//if linked list is not empty
+		if(this.size >= 1) {
+			this.tail.next = nn;
+		}
+		
+		//summary object
+		//basically where will head and tail will point under which scenario
+		
+		if(this.size ==0) {
+			this.head = this.tail = nn;
+			this.size++;
+		}else {
+			this.tail = nn;
+			this.size++;
+		}
+	}
+	
+	public void addFirst(int item) {
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		
+		if(this.size >=1) {
+			nn.next = head;
+		}
+		
+		if(this.size == 0) {
+			this.head = this.tail = nn;
+			this.size++;
+		}else {
+			this.head = nn; 
+			this.size++;
 		}
 	}
 
---
+++ linkedlist/LlClient.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class LlClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.addFirst(10);
+	 
+		ll.addLast(20);
+		ll.addLast(30);
+	 
+		ll.addFirst(40);
+		ll.addFirst(50);
+		ll.display();
+		
+
+	}
+
+}
