150afc0c62760c1be88e8ade42bab19ead60d717
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-11 20:36:05 +0530

    Linked List As a Stack

--- linkedlist/LinkedListAsStack.java
+++ linkedlist/LinkedListAsStack.java
@@ -1,10 +1,41 @@
 package linkedlist;
 
 public class LinkedListAsStack {
-
-	public static void main(String[] args) {
+		 private LinkedList stack ;
+		 
+		 public LinkedListAsStack() {
+			 this.stack = new LinkedList();
+		 }
+		 
+		 public void push(int val) {
+			 this.stack.addFirst(val);
+		 }
+		 
+		 public int pull() throws Exception{
+			 return this.stack.removeFirst();
+		 }
+		 
+		 public void top() throws Exception {
+			 this.stack.getFirst();
+		 }
+		 
+		 public void display() {
+			 this.stack.display();
+		 }
+		 
+		 public int size() {
+			 return this.stack.size();
+		 }
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		LinkedListAsStack ls = new LinkedListAsStack();
+		ls.push(10);
+		ls.display();
+		ls.push(20);
+		ls.display();
+		ls.top();
+		System.out.println(ls.pull());
+		ls.display();
 	}
 
 }
