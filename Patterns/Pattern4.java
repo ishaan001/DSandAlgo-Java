df412c2da59d0e2cf199cfe87cf1ad3518db0bdf
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-18 08:07:45 +0530

    basics changed to pattern

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,54 @@
+package Patterns;
+
+
+import java.util.Scanner;
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int rows = 1;
+		int nst = n/2;
+		int nsp =1;
+		
+		while(rows <= n) {
+			
+			int cst = 1;
+			while(cst <= nst) {
+				System.out.print("+");
+				cst++;
+			}
+			
+			int csp = 1;
+			while(csp <= nsp) {
+				System.out.print(" ");
+				csp++;
+				
+			}
+			
+			cst = 1;
+			while(cst <= nst) {
+				System.out.print("+");
+				cst++;
+			}
+			
+			
+			System.out.println();
+			if(rows <= n/2){
+				nst = nst -1;
+				nsp += 2;
+			}
+			else {
+				nst +=1;
+				nsp -=2;
+			}
+			
+			rows += 1;
+		}
+
+	}
+
+}
---
+++ Patterns/Pattern5.java
@@ -0,0 +1,50 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int rows = 1;
+		int nsp = n/2;
+		int nst =1;
+		
+		while(rows <= n) {
+			
+			int csp = 1;
+			while(csp <= nsp) {
+				System.out.print(" ");
+				csp++;
+			}
+			
+			int cst = 1;
+			while(cst <= nst) {
+				System.out.print("+");
+				cst++;
+				
+			}
+			
+			 
+			
+			
+			System.out.println();
+			if(rows <= n/2){
+				nsp = nsp -1;
+				nst += 2;
+			}
+			else {
+				nsp +=1;
+				nst -=2;
+			}
+			
+			rows += 1;
+		}
+
+	}
+
+}
