bf2a851a3050cabbb6455bc410734ee2284cf918
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-08-24 04:28:34 +0530

    queue using stack enqueue effecient approach

---
+++ Queue/QueueUsingStackEnqueueEffecient.java
@@ -0,0 +1,75 @@
+package Queue;
+
+import java.util.Stack;
+
+public class QueueUsingStackEnqueueEffecient {
+	private Stack<Integer> primary = new Stack<>();
+	private Stack<Integer> secondary = new Stack<>();
+	public void dequeue() {
+		while(primary.size() != 1) {
+			int val = primary.peek();
+			primary.pop();
+			secondary.push(val);
+		
+		}
+		primary.pop();
+		while(!secondary.isEmpty()) {
+			primary.push(secondary.peek());
+			secondary.pop();
+		}
+			
+	}
+	public int front() {
+
+		while(primary.size() != 1) {
+			int val = primary.peek();
+			primary.pop();
+			secondary.push(val);
+		
+		}
+		int front = primary.peek();
+		while(!secondary.isEmpty()) {
+			primary.push(secondary.peek());
+			secondary.pop();
+		}
+			
+	
+		return front;
+	}
+	public void enqueue(int item) {
+		primary.push(item);
+	}
+	public void display() {
+		while(!primary.isEmpty()) {
+			int val = primary.peek();
+			primary.pop();
+			secondary.push(val);
+		
+		}
+		while(!secondary.isEmpty()) {
+			System.out.print(secondary.peek()+" ");
+			primary.push(secondary.peek());
+			secondary.pop();
+		}
+		System.out.println();
+			
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QueueUsingStackEnqueueEffecient q = new QueueUsingStackEnqueueEffecient();
+		q.enqueue(10);
+		q.enqueue(20);
+		q.enqueue(30);
+		q.enqueue(40);
+		q.display();
+		q.dequeue();
+		q.display();
+		System.out.println(q.front());
+		q.enqueue(50);
+		q.display();
+		
+
+	}
+
+}
