0c612ceb17bb6459aaca63a5fceec04c39369bb0
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-01 04:04:15 +0530

    removinif duplicates

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -314,7 +314,24 @@ public class LinkedList {
 
 
     }
-
+	public  void removeDuplicated() {
+		Node curr = head; 
+		  
+        /* Traverse list till the last node */
+        while (curr != null) { 
+             Node temp = curr; 
+            /*Compare current node with the next node and  
+            keep on deleting them until it matches the current  
+            node data */
+            while(temp!=null && temp.data==curr.data) { 
+                temp = temp.next; 
+            } 
+            /*Set current node next to the next different  
+            element denoted by temp*/
+            curr.next = temp; 
+            curr = curr.next; 
+        } 
+	}
 }
 
 
---
+++ linkedlist/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+ 
+
+public class RemoveDuplicatesFromLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList();
+		ll.addLast(10);
+		ll.addLast(20);
+		ll.addLast(20);
+		ll.addFirst(10);
+		ll.addLast(30);
+		ll.display();
+		ll.removeDuplicated();
+		ll.display();
+	
+		
+		
+
+	}
+
+	
+		
+		
+
+
+}
