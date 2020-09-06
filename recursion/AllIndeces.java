171b12fbcb9868f97beb99d31337638937384eec
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-07 02:42:11 +0530

    all indeces

--- recursion/AllIndeces.java
+++ recursion/AllIndeces.java
@@ -10,10 +10,33 @@ public class AllIndeces {
 		int si=0,count=0;
 		int data=4;
 		int[] arr1= allindex(arr,si,data,count);
+		int[] arr2 = diffApproach(arr,si,data,count);
 		
 		for(int i=0; i<arr1.length;i++)
 			System.out.print(arr1[i]+" ");
 		
+		System.out.println();
+		for(int i=0; i<arr2.length;i++)
+			System.out.print(arr2[i]+" ");
+		
+	}
+
+	private static int[] diffApproach(int[] arr, int si, int data, int count) {
+		// TODO Auto-generated method stub
+		if(si == arr.length) {
+			return new int[count];
+		}
+		int[] indeces = null;
+		if(arr[si] == data) {
+			indeces = diffApproach(arr, si+1, data, count+1);
+		}else {
+			indeces = diffApproach(arr, si+1, data, count);
+		}
+		
+		if(arr[si] == data) {
+			indeces[count] = si;
+		}
+		return indeces;
 	}
 
 	private static int[] allindex(int[] arr, int si, int data, int count) {
@@ -31,5 +54,7 @@ public class AllIndeces {
 			arr2[count-1] = si;
 		return arr2;
 	}
+	
+	
 
 }
