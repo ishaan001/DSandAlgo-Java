6357aff5c6de4a6557811abb27de42b7bea7ed92
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-15 04:18:47 +0530

    find first occurace and last occurance of the element

--- recursion/FindingIndexes.java
+++ recursion/FindingIndexes.java
@@ -1,13 +1,33 @@
 package recursion;
 
-public class FindFirstIndex {
+public class FindingIndexes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {6, 8, 1, 1, 8, 3, 4};
-		int data = 4;
+		int data = 8;
 		int firstIndex = findFirstIndex(arr,0,data);
 		System.out.println(firstIndex);
+		int lastIndex = findLastIndex(arr,0,data);
+		System.out.println(lastIndex);
+
+	}
+
+	private static int findLastIndex(int[] arr, int i, int data) {
+		// TODO Auto-generated method stub
+		if(i == arr.length) {
+			return -1;
+		}
+		int  index = findLastIndex(arr, i+1, data);
+		if(index == -1) {
+			if(arr[i] == data)
+				return i;
+			else
+				return -1;
+				
+		}
+		else
+			return index;
 
 	}
 
