417ea9edd673bed7eba69cd77ec8e0e490e451be
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-15 04:40:46 +0530

    pattern printing

--- recursion/Pattern.java
+++ recursion/Pattern.java
@@ -9,8 +9,24 @@ public class Pattern {
 		Scanner sc = new Scanner(System.in);
 		int n  = sc.nextInt();
 		int rows = 1,col=1;
-		pattern(n,rows,col);
+		//pattern(n,rows,col);
+		PrintPatternAgain(n,rows,col);
 
+	}
+	private static void PrintPatternAgain(int n, int rows, int col) {
+		// TODO Auto-generated method stub
+		if(rows == n+1) {
+			return;
+		}
+		
+		if(rows >= col) {
+			System.out.print("* ");
+			PrintPatternAgain(n, rows, col+1);
+		}
+		else {
+			System.out.println();
+			PrintPatternAgain(n, rows+1, 1);
+		}
 	}
 	/* pattern printing
 	 * 
