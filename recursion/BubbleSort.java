f9bb07f6569403e6f0df1c433b16604dbcebf747
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-16 03:46:38 +0530

    bubble sort recursion

--- recursion/BubbleSort.java
+++ recursion/BubbleSort.java
@@ -5,8 +5,29 @@ public class BubbleSort {
 		int[] arr = {50,40,30,20,10};
 		 
 		bubble(arr,0,arr.length-1);
+		bubbleSort(arr,0,arr.length-1);
 		for(int i =0;i<=arr.length-1;i++)
 			System.out.print(arr[i]+" ");
+	}
+
+	private static void bubbleSort(int[] arr, int i, int j) {
+		// TODO Auto-generated method stub
+		if(j == 0)
+			return;
+		
+		if(i == j) {
+			bubbleSort(arr, 0, j-1);
+			return;
+		}
+		
+		if(arr[i] > arr[i+1])
+		{
+			int temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+		}
+		bubbleSort(arr, i+1, j);
+		
 	}
 
 	private static void bubble(int[] arr, int si, int li) {
