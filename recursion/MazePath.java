52fd8793159219beffbcc64f03a0db003e5c0838
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-18 05:12:28 +0530

    count MAze path

--- recursion/MazePath.java
+++ recursion/MazePath.java
@@ -15,9 +15,37 @@ public class MazePath {
 		for (String p : path1) {
 			System.out.print(p+" ");
 		}
-
+		System.out.println("\ncount added ----------------");
+		int countMaze = getCountMazePathDiagonal(0, 0, 2, 2);
+		System.out.println(countMaze);
 	}
 	
+	public static int getCountMazePathDiagonal(int cr, int cc, int er, int ec) {
+		// TODO Auto-generated method stub
+		if(cr == er && cc == ec) {
+			//br = baseResult +ve cases
+			return 1;
+		}
+		
+		if(cr > er || cc > ec) {
+			//br = baseResult -ve cases
+			 
+			return 0;
+		}
+		int count = 0;
+		 
+		///for horizontal movement rrh = recurciveResultHorizontal
+		count += getCountMazePathDiagonal(cr, cc+1, er, ec);
+		 
+		// for vertical movement rrv = recurciveResultVertical
+		count += getCountMazePathDiagonal(cr+1, cc, er, ec);
+		 
+		// for diagonal movement rrd = recurciveResultDiagonal
+		count += getCountMazePathDiagonal(cr+1, cc+1, er, ec);
+		 
+		return count;
+	}
+
 	/*
 	 * In how many ways we can reach from one pt of matrix to another 
 	 * in the condition where 1 Horizontal and 1 Vertical and 1Diagonal movement is allowed at time */
