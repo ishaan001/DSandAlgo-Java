669f286e020b8448f924df04311ea351cb273d0d
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-30 03:23:56 +0530

    1D queens combinations

--- Backtracking/BackTracking_Queens.java
+++ Backtracking/BackTracking_Queens.java
@@ -8,10 +8,12 @@ public class BackTracking_Queens {
 		// TODO Auto-generated method stub
 		Boolean[] b = new Boolean[4];
 		Arrays.fill(b,false);
-		queenPermutations(b, 0, 2, "");
-
+		//queenPermutations(b, 0, 2, "");
+			queenCombinations(b, 0, 2, "", -1);
 	}
 	static int count = 0;
+	// qpsf - > quuens placed so far
+	//tq -> total queens
 	public static void queenPermutations(Boolean[] boxes, int qpsf, int tq, String ans) {
 
 		if(qpsf == tq ) {
@@ -28,4 +30,20 @@ public class BackTracking_Queens {
 		}
 	}
 
+	static int counter =0;
+	public static void queenCombinations(Boolean[] boxes, int qpsf, int tq, String ans, int lastBoxUsed) {
+		if(qpsf == tq) {
+			counter++;
+			System.out.println(counter+"  "+ans);
+			return;
+			
+		}
+		for(int i =lastBoxUsed + 1; i <boxes.length; i++) {
+			if(boxes[i] == false) {
+				boxes[i] = true;
+				queenCombinations(boxes, qpsf + 1, tq, ans + "q" + qpsf + "b" + i +" ", i);
+				boxes[i] = false;
+			}
+		}
+	}
 }
