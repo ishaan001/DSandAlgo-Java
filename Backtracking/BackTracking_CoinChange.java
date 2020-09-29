337db6ec079a3464f86cff3fc865fb21f3f03971
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-09-30 04:59:23 +0530

    coin chnage

---
+++ Backtracking/BackTracking_CoinChange.java
@@ -0,0 +1,45 @@
+package Backtracking;
+
+public class BackTracking_CoinChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] denominations = {2, 3, 5, 6};
+		
+	//	coinChangeCombination(denominations, 10, "", 0);
+		coinChangePermutations(denominations, 10, "");
+	}
+
+	static int count = 0;
+	
+	public static void coinChangeCombination(int[] denominations, int amt, String ans, int lastDenominationIdx) {
+		if(amt == 0) {
+			count++;
+			System.out.println(count+". "+ans);
+			return;
+		}
+		
+		
+		for(int i = lastDenominationIdx; i<denominations.length; i++) {
+			if(amt >= denominations[i]) {
+				coinChangeCombination(denominations, amt - denominations[i], ans + denominations[i] + " ", i);
+			}
+		}
+	}
+	static int counter = 0;
+	public static void coinChangePermutations(int[] denominations, int amt, String ans) {
+		if(amt == 0) {
+			counter++;
+			System.out.println(counter+". "+ans);
+			return;
+		}
+		
+		
+		for(int i = 0; i<denominations.length; i++) {
+			if(amt >= denominations[i]) {
+				coinChangePermutations(denominations, amt - denominations[i], ans + denominations[i] + " ");
+			}
+		}
+	}
+
+}
