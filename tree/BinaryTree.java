a9e627a73bf81eb6b629365b7cdac835db2d2833
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-10-16 21:26:17 +0530

    tree traversal tech

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -1,7 +1,12 @@
 package tree;
 
+import java.lang.reflect.Array;
 import java.security.spec.DSAGenParameterSpec;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTree {
 	private class Node {
@@ -85,23 +90,120 @@ public class BinaryTree {
 			display(node.right);
 
 	}
-	
+
 	public int height() {
 		return height(this.root);
 	}
 
 	private int height(Node node) {
 		// TODO Auto-generated method stub
-		if(node == null) {
+		if (node == null) {
 			return -1;
 		}
-		
+
 		int leftHeight = height(node.left);
 		int rightheight = height(node.right);
-		
+
 		int height = Math.max(leftHeight, rightheight) + 1;
-		
+
 		return height;
 	}
 
+	public void preOrderTraversal() {
+		preOrderTraversal(this.root);
+	}
+
+	private void preOrderTraversal(Node node) {
+		// TODO Auto-generated method stub
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.data + " ");
+		preOrderTraversal(node.left);
+		preOrderTraversal(node.right);
+
+	}
+
+	public void postOrderTraversal() {
+		postOrderTraversal(this.root);
+	}
+
+	private void postOrderTraversal(Node node) {
+		// TODO Auto-generated method stub
+		if (node == null) {
+			return;
+		}
+		postOrderTraversal(node.left);
+		postOrderTraversal(node.right);
+		System.out.print(node.data + " ");
+
+	}
+
+	public void inOrderTraversal() {
+		inOrderTraversal(this.root);
+	}
+
+	private void inOrderTraversal(Node node) {
+		// TODO Auto-generated method stub
+		if (node == null) {
+			return;
+		}
+		inOrderTraversal(node.left);
+		System.out.print(node.data + " ");
+		inOrderTraversal(node.right);
+
+	}
+
+	public void inOrderTraversalItreatively() {
+		ArrayList<Integer> ll = inOrderTraversalItreatively(this.root);
+		System.out.println(ll);
+	}
+
+	private ArrayList<Integer> inOrderTraversalItreatively(Node node) {
+		// TODO Auto-generated method stub
+		if(node == null) {
+			ArrayList<Integer> ll = new ArrayList();
+			return ll;
+		}
+		Stack<Node> s = new Stack<>();
+		ArrayList<Integer> ll = new ArrayList();
+		while (true) {
+			if (node != null) {
+				s.push(node);
+				node = node.left;
+			} else {
+				if (s.isEmpty()) {
+					break;
+				} else {
+					node = s.pop();
+					ll.add(node.data);
+					node = node.right;
+				}
+			}
+		}
+		
+		return ll;
+
+	}
+
+	public void levelOrderTraversal() {
+		levelOrderTraversal(this.root);
+	}
+
+	private void levelOrderTraversal(Node node) {
+		// TODO Auto-generated method stub
+		Queue<Node> q = new LinkedList<BinaryTree.Node>();
+		q.add(node);
+		while (!q.isEmpty()) {
+			Node remove = q.remove();
+			System.out.print(remove.data + " ");
+
+			if (remove.left != null)
+				q.add(remove.left);
+
+			if (remove.right != null)
+				q.add(remove.right);
+		}
+
+	}
 }
--- tree/BinaryTreeClient.java
+++ tree/BinaryTreeClient.java
@@ -4,8 +4,20 @@ public class BinaryTreeClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false 
 		BinaryTree br = new BinaryTree();
 		br.display();
+		System.out.println(br.height());
+		System.out.println("=========Pre Order Traversal============");
+		br.preOrderTraversal();
+		System.out.println("\n=========Post Order Traversal============");
+		br.postOrderTraversal();
+		System.out.println("\n=========In Order Traversal============");
+		br.inOrderTraversal();
+		System.out.println("\n=========Level Order Traversal============");
+		br.levelOrderTraversal();
+		System.out.println("\n=========In Order Traversal loop============");
+		br.inOrderTraversalItreatively();
 
 	}
 
